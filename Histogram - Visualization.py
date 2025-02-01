58247c5cd8063e7b300b505daeb2f776e89b018a
debora-kchw <m6.debora@gmail.com>
2025-02-01 16:37:56 +0100

    Update Histogram - Visualization.py

--- Histogram - Visualization.py
+++ Histogram - Visualization.py
@@ -6,18 +6,23 @@ import seaborn as sns
 df = pd.read_excel("Thesis Data.xlsx", "Sheet2")
 
 ##Relax the limit on display columns and rows
+##Only in the case of big datasets
 pd.set_option("display.max_columns", 2)
 pd.set_option("display.max_rows", 13)
 
 #This enters the data frame and lets you select a specific column
 WBC = df["WBC"]
+
 #This lets you preview the data, helps in the instances of many samples
 print(WBC.head())
+
 #Prints the entirety of that column
 print(WBC)
 print(df)
 
+##Call on seaborn to create histogram
 sns.histplot(WBC,bins=30, kde=True, color="lightgreen", edgecolor="blue")
+##Changes label names and title
 plt.xlabel('WBCs (10^3/uL)')
 plt.ylabel('# of individuals')
 plt.title('White Blood Cells')
